982aa0d9fcb829237baeb1317752711e2ae7bbc6
mrgreen <mrgreen@archbang.org>
2015-05-01 07:25:33 +0100

    change too rc release scheme

--- mklive.sh
+++ mklive.sh
@@ -341,7 +341,7 @@ _linux_series=$($XBPS_QUERY_CMD -r $ROOTFS ${XBPS_REPOSITORY:=-R} -x linux)
 KERNELVERSION=$($XBPS_QUERY_CMD -r $ROOTFS ${XBPS_REPOSITORY:=-R} -p pkgver ${_linux_series})
 KERNELVERSION=$($XBPS_UHELPER_CMD getpkgversion $KERNELVERSION)
 
-: ${OUTPUT_FILE="springlinux-${BASE_ARCH}-$(date +%d%m%y).iso"}
+: ${OUTPUT_FILE="springlinux-rc-${BASE_ARCH}-$(date +%d%m%y).iso"}
 
 info_msg "[2/9] Installing software to generate the image: ${REQUIRED_PKGS} ..."
 install_prereqs
